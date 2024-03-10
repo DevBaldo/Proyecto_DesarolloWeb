1ab9387fb83de10c2201e0d9bcd16712f031ddb3
DevBaldo <135394363+DevBaldo@users.noreply.github.com>
2024-03-10 15:48:44 -0600

    Base
    Base del proyecto

---
+++ Proyecto/src/test/java/com/grupo6/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
